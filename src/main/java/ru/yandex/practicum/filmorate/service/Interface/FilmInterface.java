5343012af3cd1d93cd018aca14dca8b29adf6765
Sergey <Melkii914@mail.ru>
2023-04-23 11:52:12 +0300

    fix: test added1

--- src/main/java/ru/yandex/practicum/filmorate/service/Interface/FilmInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/Interface/FilmInterface.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface FilmInterface {
     Film addFilm(int id,Film film);
+
     Film updateFilm(int id,Film film);
+
     List<Film> getFilms();
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/Interface/UserInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/Interface/UserInterface.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface UserInterface {
     User addUser(int id,User user);
+
     User updateUser(int id,User user);
+
     List<User> getUsers();
 }
